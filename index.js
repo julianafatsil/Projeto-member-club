9e9d4af81ebfd7e6bb421216155d7fa72594525f
Juliana da Silva <julianafatsil@gmail.com>
2024-08-05 00:58:44 -0300

    Created perfil-info-client

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ async function fetchClientById(id) {
       }
       const response = await fetch(`http://localhost:3000/clients/${id}`);
       const data = await response.json()
-      console.log(data)
+      createDataClient(data);
 
   } catch (error) {
     alert("Cliente não encontrado!")
@@ -19,4 +19,22 @@ const cartId = document.getElementById("id-cart");
 addEventListener("submit", (event) => {
   event.preventDefault()
 fetchClientById(cartId.value)
-})
+});
+
+function createDataClient(data){
+  const main = document.getElementById("data-client");
+  console.log(data);
+  const divPerfilClient = document.createElement('div');
+  divPerfilClient.className = "perfil-client";
+  const divInfoClient = document.createElement('div');
+  divInfoClient.className = "info-client";
+  const h1InfoClientName = document.createElement('h1');
+  h1InfoClientName.innerText = data.name;
+  const pInfoClientDate = document.createElement("p");
+  pInfoClientDate.innerText = (`Cliente desde ${data.clientSince}`)
+  divInfoClient.appendChild(h1InfoClientName);
+  divInfoClient.appendChild(pInfoClientDate);
+  divPerfilClient.appendChild(divInfoClient);
+
+  main.appendChild(divPerfilClient);
+}
